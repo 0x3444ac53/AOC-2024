789cda8cfe0de1af633ae5e497ec57f183d2b2b1
Nora <elliemaegalyean@gmail.com>
2024-12-03 11:34:03 -0500

    part 2: Thanks, I hate it

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -9,19 +9,22 @@ impl IsSafe for Vec<u32> {
         let mut changes: Vec<i32> = vec![];
         self.windows(2)
             .for_each(|w| changes.push(w[1] as i32 - w[0] as i32));
-        let _safe = changes.clone().into_iter().filter(|w| *w <= 3 && *w >= -3);
+        let _safe = changes
+            .clone()
+            .into_iter()
+            .map(|w| (-3..=3).contains(&w) && w != 0)
+            .collect::<Vec<bool>>();
+        let num_of_bad_changes = _safe.iter().filter(|&w| *w).count().abs_diff(changes.len());
         if (changes.iter().all(|w| *w > 0) || changes.iter().all(|w| *w < 0))
-            && _safe.clone().count() == changes.len()
+            && num_of_bad_changes == 0
         {
             true
+        } else if !damped {
+            return false;
         } else {
-            dbg!(self);
-            dbg!(changes.clone());
-            dbg!(
-                (changes.iter().all(|w| *w > 0) || changes.iter().all(|w| *w < 0))
-                    && _safe.count() == changes.len()
-            );
-            false
+            (0..self.len())
+                .map(|i| self.iter().take(i).chain(self.iter().skip(i + 1)))
+                .any(|w| w.copied().collect::<Vec<u32>>().is_safe(false))
         }
     }
 }
@@ -41,7 +44,17 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    input.lines().try_fold(0, |acc, line| {
+        let nums: Vec<u32> = line
+            .split(' ')
+            .map(|n| n.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        if nums.is_safe(true) {
+            Some(acc + 1)
+        } else {
+            Some(acc)
+        }
+    })
 }
 
 #[cfg(test)]
@@ -57,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
